338b9261d3b80297ee9b7a7d4d3294391e95f651
aniltexascowboy777 <aniltexascowboy@gmail.com>
2019-06-20 14:37:45 -0500

    Added some files
    Test

---
+++ MongoDB_3sql_yevhen.sql
@@ -0,0 +1,9 @@
+SELECT FLATTEN * FROM clothes LIMIT 1000
+GO
+
+
+select * from clothes
+GO
+
+select flatten * from clothes
+go
---
+++ amazon_2sql.sql
@@ -0,0 +1,6 @@
+
+SELECT * FROM public.alumni
+GO
+
+SELECT * FROM public.orders
+GO
